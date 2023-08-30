21ba58cf099c4ee764933d9f69abedb51fbc7580
Anthony <bondolo90@gmail.com>
2023-08-30 11:49:21 -0700

    Commit 10

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -10,10 +10,10 @@
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree && func)
-		int b = tree->n;
+	if (!tree || !func)
+		return;
 
-		binary_tree_inorder(tree->left, func);
-		func(b);
-		binary_tree_inorder(tree->right, func);
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
